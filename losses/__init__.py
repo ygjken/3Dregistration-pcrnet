3e66be2f04d8bfcda7a9438a8c1de7a7904f63bb
ygjken <ken.748322@gmail.com>
2022-01-24 00:37:11 +0900

    :sparkles:add EMDLoss

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,8 +1,2 @@
-try:
-    from .emd import EMDLoss
-except:
-    print("Sorry EMD loss is not compatible with your system!")
-try:
-    from .chamfer_distance import ChamferDistanceLoss
-except:
-    print("Sorry ChamferDistance loss is not compatible with your system!")
+from .emd import EarthMoverDistanceFunction, earth_mover_distance
+from .chamfer_distance import ChamferDistanceLoss
